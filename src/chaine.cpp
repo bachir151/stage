ee970fa06b36a8b895086dc1cd40838877c82414
bachir151 <elbachir90@gmail.com>
2015-06-30 11:24:55 +0200

    Create chaine.cpp

---
+++ src/chaine.cpp
@@ -0,0 +1,54 @@
+/**
+ * @brief     Chaine.h
+ * @date      June 29, 2015
+ * @author    Diedhiou Ahmed Bachir
+ * @version   1.1
+ */
+ 
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include "Chaine.h"
+using namespace std;
+
+
+
+/**
+ *@returns sorted string in alphabetic order
+ */
+string Chaine::Tri( ){
+ 	std::sort(mot.begin(), mot.end());
+	return (mot);
+ }
+
+/**
+ *@return the string 
+ */
+ 
+string Chaine::getMot(){
+	return mot ;
+}
+
+
+/**
+ *sets the string 
+ */
+ 
+void Chaine::setMot(string mot){
+	this->mot=mot;
+}
+
+
+/**
+ *return true if string is a palindrome  
+ */
+
+string Chaine::Estpalindrom(){
+
+if(equal(mot.begin(), mot.end(), mot.rbegin()))
+
+        return "true" ; 
+	else 
+		return "false" ; 
+}
+
